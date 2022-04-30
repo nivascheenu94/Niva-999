5d9f86ba4788e36245fb118a34d42a706e48b961
nivascheenu94 <srinivasnivas180@gmail.com>
2022-04-30 08:22:39 +0530

    Testing done

--- src/org/compmanufacturer/BrandManufactures.java
+++ src/org/compmanufacturer/BrandManufactures.java
@@ -2,12 +2,14 @@ package org.compmanufacturer;
 
 public abstract class BrandManufactures extends InternetConn{
 public void Dell() {
+	System.out.println("Dell is the 2 nd best Manufacturer or laptop");
 
 }
 public void HP() {
-
+System.out.println("HP is the  best Manufacturer or laptop");
 }
 public void Acer() {
+	System.out.println("Acer is the  best Manufacturer of laptop in GAMING genre");
 
 }
 }
